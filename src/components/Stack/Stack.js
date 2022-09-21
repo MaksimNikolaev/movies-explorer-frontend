b91e8f6f5c2caf6d5b4f98c7f050a490c41eaa98
Maksim Nikolaev <mksniklaev@gmail.com>
2022-09-21 23:46:57 +0500

    feat: adaptive landing

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import Title from '../Title/Title'
 import './Stack.css'
 
@@ -11,25 +10,25 @@ const Stack = () => {
         <p className='stack__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
         <ul className='stack__list'>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/HTML' className='stack__link' target='_blank'>HTML</a>
+            <a href='https://ru.wikipedia.org/wiki/HTML' className='stack__link' target='blank'>HTML</a>
           </li>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/CSS' className='stack__link' target='_blank'>CSS</a>
+            <a href='https://ru.wikipedia.org/wiki/CSS' className='stack__link' target='blank'>CSS</a>
           </li>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/JavaScript' className='stack__link' target='_blank'>JS</a>
+            <a href='https://ru.wikipedia.org/wiki/JavaScript' className='stack__link' target='blank'>JS</a>
           </li>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/React' className='stack__link' target='_blank'>React</a>
+            <a href='https://ru.wikipedia.org/wiki/React' className='stack__link' target='blank'>React</a>
           </li>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/Git' className='stack__link' target='_blank'>Git</a>
+            <a href='https://ru.wikipedia.org/wiki/Git' className='stack__link' target='blank'>Git</a>
           </li>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/Express_(%D1%84%D1%80%D0%B5%D0%B9%D0%BC%D0%B2%D0%BE%D1%80%D0%BA)' className='stack__link' target='_blank'>Express.js</a>
+            <a href='https://ru.wikipedia.org/wiki/Express_(%D1%84%D1%80%D0%B5%D0%B9%D0%BC%D0%B2%D0%BE%D1%80%D0%BA)' className='stack__link' target='blank'>Express.js</a>
           </li>
           <li className='stack__item'>
-            <a href='https://ru.wikipedia.org/wiki/MongoDB' className='stack__link' target='_blank'>mongoDB</a>
+            <a href='https://ru.wikipedia.org/wiki/MongoDB' className='stack__link' target='blank'>mongoDB</a>
           </li>
         </ul>
         </div>
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -7,19 +7,21 @@ const Student = () => {
   return (
     <section className='student'>
       <Title section='Студент'/>
-      <h3 className='student__name'>Максим</h3>
-      <p className='student__job'>Фронтенд-разработчик, 30 лет</p>
-      <p className='student__description'>Я работаю и живу в г.Стерлитамак. Так как мне нравится программирование,
-      решил пойти в сферу фронтенд разработки. Фронтенд разработка нравится тем, что написав пару строчек кода, уже
-      можно увидеть результат своей работы в веб браузере. Текущий год я выполнял различные задачи на обучении от
-      Яндекс Практикума. Обучение осуществлялось с наставником и проекты регулярно проходили ревью, поэтому я получил
-      огромный опыт. Люблю активный отдых, поездки, путешествия, уделяю свободное время обучению.</p>
-      <ul className='student__contact'>
-        <li className='student__item'><a className='student__contact-link' href='https://github.com/MaksimNikolaev'>Github</a></li>
-        <li className='student__item'><a className='student__contact-link' href='https://t.me/NikolaevMaks'>Telegram</a></li>
-        <li className='student__item'><a className='student__contact-link' href='https://vk.com/id170680365'>Вконтакте</a></li>
-      </ul>
-      <img className='student__photo' src={photo} alt='Моя фотография'/>
+      <div className='student__info'>
+        <h3 className='student__name'>Максим</h3>
+        <p className='student__job'>Фронтенд-разработчик, 30 лет</p>
+        <p className='student__description'>Я работаю и живу в г.Стерлитамак. Так как мне нравится программирование,
+        решил пойти в сферу фронтенд разработки. Фронтенд разработка нравится тем, что написав пару строчек кода, уже
+        можно увидеть результат своей работы в веб браузере. Текущий год я выполнял различные задачи на обучении от
+        Яндекс Практикума. Обучение осуществлялось с наставником и проекты регулярно проходили ревью, поэтому я получил
+        огромный опыт. Люблю активный отдых, поездки, путешествия, уделяю свободное время обучению.</p>
+        <ul className='student__contact'>
+          <li className='student__item'><a className='student__contact-link' href='https://github.com/MaksimNikolaev'>Github</a></li>
+          <li className='student__item'><a className='student__contact-link' href='https://t.me/NikolaevMaks'>Telegram</a></li>
+          <li className='student__item'><a className='student__contact-link' href='https://vk.com/id170680365'>Вконтакте</a></li>
+        </ul>
+        <img className='student__photo' src={photo} alt='Моя фотография'/>
+      </div>
       <Portfolio/>
     </section>
   )
