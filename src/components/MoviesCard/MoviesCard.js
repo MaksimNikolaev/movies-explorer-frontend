727e109e137a56304994eaf7489f02182eb75914
Maks <mksniklaev@gmail.com>
2022-10-14 20:56:14 +0500

    feat: add link on trailer

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,13 @@
 import { useLocation } from "react-router-dom";
 import "./MoviesCard.css";
 
-const MoviesCard = ({ movie, duration, saved, handleSavesMovies, handleDeleteMovies }) => {
+const MoviesCard = ({
+  movie,
+  duration,
+  saved,
+  handleSavesMovies,
+  handleDeleteMovies,
+}) => {
   const { pathname } = useLocation();
 
   const onHandleSavesMovies = (e) => {
@@ -11,38 +17,45 @@ const MoviesCard = ({ movie, duration, saved, handleSavesMovies, handleDeleteMov
 
   const onHandleDeleteMovies = (e) => {
     e.preventDefault();
-    handleDeleteMovies(movie)
+    handleDeleteMovies(movie);
   };
+
   return (
     <li className="movies-card__item">
-      <img
-        src={
-          pathname === "/saved-movies"
-            ? `${movie.image}`
-            : `https://api.nomoreparties.co${movie.image.url}`
-        }
-        className="movies-card__img"
-        alt={movie.nameRU}
-      />
-      <div className="movies-card__info">
-        <h2 className="movies-card__name">{movie.nameRU}</h2>
-        <p className="movies-card__duration">{duration}</p>
-      </div>
-      <button
-        className={`movies-card__btn ${
-          pathname === "/saved-movies" ?
-          "movies-card__btn_type_active" :
-          saved
-            ? "movies-card__btn_type_active"
-            : "movies-card__btn_type_inactive"
-        }`}
-        type="button"
-        onClick={
-          pathname === "/saved-movies" ? onHandleDeleteMovies : saved ? onHandleDeleteMovies : onHandleSavesMovies
-      }
-      >
-        Сохранить
-      </button>
+      <a href={movie.trailerLink} className="movies-card__link" target='blank'>
+        <img
+          src={
+            pathname === "/saved-movies"
+              ? `${movie.image}`
+              : `https://api.nomoreparties.co${movie.image.url}`
+          }
+          className="movies-card__img"
+          alt={movie.nameRU}
+        />
+        <div className="movies-card__info">
+          <h2 className="movies-card__name">{movie.nameRU}</h2>
+          <p className="movies-card__duration">{duration}</p>
+        </div>
+        <button
+          className={`movies-card__btn ${
+            pathname === "/saved-movies"
+              ? "movies-card__btn_type_active"
+              : saved
+              ? "movies-card__btn_type_active"
+              : "movies-card__btn_type_inactive"
+          }`}
+          type="button"
+          onClick={
+            pathname === "/saved-movies"
+              ? onHandleDeleteMovies
+              : saved
+              ? onHandleDeleteMovies
+              : onHandleSavesMovies
+          }
+        >
+          Сохранить
+        </button>
+      </a>
     </li>
   );
 };
